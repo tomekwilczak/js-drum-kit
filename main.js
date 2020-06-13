348dfa28e3ac4c66b82f1c2dcd003f28ac517f2f
Tomek Wilczak <tomek.wilczak@gmail.com>
2020-06-13 18:55:04 +0200

    Added object for all sounds

--- main.js
+++ main.js
@@ -1,14 +1,24 @@
 // Defining sound files
-const aSound = new Audio("sounds/clap.wav");
-const sSound = new Audio("sounds/hihat.wav");
-const dSound = new Audio("sounds/kick.wav");
-const fSound = new Audio("sounds/openhat.wav");
-const gSound = new Audio("sounds/boom.wav");
-const hSound = new Audio("sounds/ride.wav");
-const jSound = new Audio("sounds/snare.wav");
-const kSound = new Audio("sounds/tom.wav");
-const lSound = new Audio("sounds/tink.wav");
-console.log(typeof aSound);
+const Sounds = {
+  aSound: new Audio("sounds/clap.wav"),
+  sSound: new Audio("sounds/hihat.wav"),
+  dSound: new Audio("sounds/kick.wav"),
+  fSound: new Audio("sounds/openhat.wav"),
+  gSound: new Audio("sounds/boom.wav"),
+  hSound: new Audio("sounds/ride.wav"),
+  jSound: new Audio("sounds/snare.wav"),
+  kSound: new Audio("sounds/tom.wav"),
+  lSound: new Audio("sounds/tink.wav"),
+};
+// const aSound = new Audio("sounds/clap.wav");
+// const sSound = new Audio("sounds/hihat.wav");
+// const dSound = new Audio("sounds/kick.wav");
+// const fSound = new Audio("sounds/openhat.wav");
+// const gSound = new Audio("sounds/boom.wav");
+// const hSound = new Audio("sounds/ride.wav");
+// const jSound = new Audio("sounds/snare.wav");
+// const kSound = new Audio("sounds/tom.wav");
+// const lSound = new Audio("sounds/tink.wav");
 
 // Defining square divs
 const aSquare = document.querySelector(".s-sound--js");
@@ -37,8 +47,7 @@ function playKey(key) {
   keyStr = keyStr.slice(3, 4);
   keyStr = keyStr.toLowerCase();
   keyStr = `${keyStr}Sound`;
-  console.log(`${keyStr}`);
-  console.log(typeof aSound);
-  eval(keyStr).play();
-  //   aSound.play(); // to działa
+  //   console.log(`${keyStr}`);
+  //   eval(keyStr).play(); // Działa ale to złe rozwiązanie, niebiezpieczna funkcja
+  Sounds[keyStr].play();
 }
