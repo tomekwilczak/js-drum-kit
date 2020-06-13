97b455021a968480c2ab6672a9c9c3f7efc145b5
Tomek Wilczak <tomek.wilczak@gmail.com>
2020-06-13 17:54:33 +0200

    Added click eventlisteners

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Defining sound files
 const aSound = new Audio("../sounds/clap.wav");
 const sSound = new Audio("../sounds/hihat.wav");
 const dSound = new Audio("../sounds/kick.wav");
@@ -6,28 +7,38 @@ const gSound = new Audio("../sounds/boom.wav");
 const hSound = new Audio("../sounds/ride.wav");
 const jSound = new Audio("../sounds/snare.wav");
 const kSound = new Audio("../sounds/tom.wav");
-const lound = new Audio("../sounds/tink.wav");
+const lSound = new Audio("../sounds/tink.wav");
+
+// Defining square divs
+const aSquare = document.querySelector(".s-sound--js");
+const sSquare = document.querySelector(".s-sound--js");
+const dSquare = document.querySelector(".d-sound--js");
+const fSquare = document.querySelector(".f-sound--js");
+const gSquare = document.querySelector(".g-sound--js");
+const hSquare = document.querySelector(".h-sound--js");
+const jSquare = document.querySelector(".j-sound--js");
+const kSquare = document.querySelector(".k-sound--js");
+const lSquare = document.querySelector(".l-sound--js");
+
+sSquare.addEventListener("click", playClick);
+
+function playClick() {
+  sSound.play();
+}
+
+// Added keypress listening for watching keyboard presses
 
 document.addEventListener("keypress", playKey);
 
-function PlaySound() {
-  dSound.play();
-  console.log("play");
-}
-
-function playKey(sound) {
-  let keyStr = sound.code;
+// Changing pressed key name to xSound string
+function playKey(key) {
+  let keyStr = key.code;
   keyStr = keyStr.slice(3, 4);
   keyStr = keyStr.toLowerCase();
   keyStr = `${keyStr}Sound`;
-  PlaySound(`${keyStr}Sound`);
+  //   keyStr.play();
   console.log(`${keyStr}`);
+  hSound.play();
 }
 
-// PlaySound(aSound);
-// setTimeout(2000);
-// PlaySound(sSound);
-// setTimeout(2000);
-// PlaySound(dSound);
-// PlaySound(fSound);
-// PlaySound(gSound);
+aSound.play();
